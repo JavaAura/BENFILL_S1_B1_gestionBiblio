8984bb00b599bab1f576e772fd300d862c273e46
Anass Benfillous <anasse.benfillous@gmail.com>
2024-09-02 16:01:30 +0100

    Update ConsoleUI.java

--- classes/ConsoleUI.java
+++ classes/ConsoleUI.java
@@ -11,12 +11,16 @@ public class ConsoleUI extends Choices {
 		while (true) {
 			switch (this.choice) {
 			case 0:
-				choice = this.home();
+				choice = this.homeUI();
 				clear();
 				break;
 			case 1:
-				choice = this.document();
+				choice = this.documentUI();
 				clear();
+				break;
+			case 4:
+				clear();
+				choice = this.displayDocsUI();
 				break;
 			case 6:
 				clear();
