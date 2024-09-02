69be09d87d37cd0c8c6b78271c7436b6bf027031
Anass Benfillous <anasse.benfillous@gmail.com>
2024-09-02 16:02:37 +0100

    handle data storing

--- data/Documents.java
+++ data/Documents.java
@@ -1,16 +1,33 @@
 package data;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import classes.Livre;
 
 public class Documents {
 
-	static HashMap<Livre, Livre> livres = new HashMap<Livre, Livre>();
+	public static HashMap<Long, Livre> livres = new HashMap<Long, Livre>();
 
-	public static void addLivre(Livre obj) {
+	public static void addLivre(long id, Livre obj) {
+		livres.put(id, obj);
+	}
 
-		livres.put(obj, obj);
+	public static void getAllDocs() {
+		if (livres.isEmpty()) {
+			System.out.println("Aucun livre disponible.");
+			return;
+		}
+		System.out.println("Liste des Livres:");
+		System.out.println("=================");
 
+		for (Map.Entry<Long, Livre> entry : livres.entrySet()) {
+			long key = entry.getKey();
+			Livre livre = entry.getValue();
+
+			System.out.println(key);
+			System.out.println(livre.afficherDetails());
+			System.out.println("=====================================");
+		}
 	}
 }
